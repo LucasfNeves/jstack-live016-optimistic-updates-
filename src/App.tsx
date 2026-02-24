c4f65f71f4e6531cca8c3d3173ab3db6e8779230
Lucas Farias <lucasfarias.ln@gmail.com>
2026-02-23 21:26:19 -0300

    feat: implement theme context and mode toggle component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { ModeToggle } from "./components/ui/ModeToglle";
+
 export function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <ModeToggle />
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App.tsx";
 import "./styles/global.css";
+import { ThemeProvider } from "./app/contexts/ThemeContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 );
