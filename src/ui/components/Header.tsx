7dadb187198dcee72d18df1778935901aa5d0a21
Lucas Farias <lucasfarias.ln@gmail.com>
2026-02-23 21:34:49 -0300

    feat: add utility functions, button, dropdown menu, header, mode toggle, and users list components

---
+++ src/ui/components/Header.tsx
@@ -0,0 +1,9 @@
+export function Hader() {
+  return (
+    <header className="w-full border-b">
+      <div className="container flex h-16 items-center justify-between">
+        <h1 className="text-lg font-bold">Vite UI</h1>
+      </div>
+    </header>
+  );
+}
